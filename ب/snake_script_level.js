eb442a6a92285e17093ce992d09568f1e4c5cf89
toxnNMisBrian <89005913+brianmatox@users.noreply.github.com>
2023-12-05 12:45:31 +0200

    Update snake_script_level.js
    12/05/23
    Added some amazing scripts to the game
    --> Pretty great js functions

--- ب/snake_script_level.js
+++ ب/snake_script_level.js
@@ -25,11 +25,41 @@ selectedGameSpeed = gameSpeed;
 let highScore = localStorage.getItem("high-score") || 0;
 highScoreElement.innerText = `High Score: ${highScore}`;
 
-const changeFoodPosition = () => {
+/*const changeFoodPosition = () => {
     //Passing a random 0 - 30 value as food position
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
+}*/
+const changeFoodPosition = () => {
+    let maxGridPos = 30; // Assuming your grid is 30x30
+    let freePositions = [];
+
+    // Generate a list of all free positions
+    for(let x = 1; x <= maxGridPos; x++) {
+        for(let y = 1; y <= maxGridPos; y++) {
+            if(!isPositionOccupiedBySnake({x, y})) {
+                freePositions.push({x, y});
+            }
+        }
+    }
+
+    // Pick a random free position for the food
+    let foodPos = freePositions[Math.floor(Math.random() * freePositions.length)];
+
+    foodX = foodPos.x;
+    foodY = foodPos.y;
 }
+
+function isPositionOccupiedBySnake(pos) {
+    for(let i = 0; i < snakeBody.length; i++) {
+        if(snakeBody[i][0] === pos.x && snakeBody[i][1] === pos.y) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 const handleGameOver = () =>{
  //clearing the timer and reloading the page on game over
     clearInterval(setIntervalId);
@@ -41,18 +71,33 @@ const changeDirection = (e) => {
     if (e.key === "ArrowUp" && velocityY !== 1) {
         velocityX = 0;
         velocityY = -1;
+        updateSnakeDirection('up');
     } else if (e.key === "ArrowDown" && velocityY !== -1) {
         velocityX = 0;
         velocityY = 1;
+        updateSnakeDirection('down');
     } else if (e.key === "ArrowLeft" && velocityX !== 1) {
         velocityX = -1;
         velocityY = 0;
+        updateSnakeDirection('left');
     } else if (e.key === "ArrowRight" && velocityX !== -1) {
         velocityX = 1;
         velocityY = 0;
+        updateSnakeDirection('right');
     } else if (e.key === " ") { // Added condition for space key
         handlePause(); // Call function to handle pause game
     }
+    
+    function updateSnakeDirection(direction) {
+        let head = document.querySelector('.head');
+        let tail = document.querySelector('.tail');
+    
+        head.classList.remove('up', 'right', 'down', 'left');
+        tail.classList.remove('up', 'right', 'down', 'left');
+    
+        head.classList.add(direction);
+        tail.classList.add(direction);
+    }    
 }
 
 // document.addEventListener("keydown", changeDirection);
@@ -113,9 +158,24 @@ const initGame = () => {
     }
 
     for (let i = 0; i < snakeBody.length; i++) {
+        let partClass = '';
+
+        // Assign 'head' class to the first element
+        if(i == 0) {
+            partClass = 'head';
+        }
+        // Assign 'tail' class to the last element
+        else if(i == snakeBody.length - 1) {
+            partClass = 'tail';
+        }
+        // Assign 'body' class to all other elements
+        else {
+            partClass = 'body';
+        }
         //const element = array[i];
-        //Adding a div for each part of the snakes body 
-        htmlMarkup += `<div class="head" style="grid-area:${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;   
+        //Adding a div for each part of the snakes body-->Adding a fancy body 12/03/2023 
+        // htmlMarkup += `<div class="head" style="grid-area:${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        htmlMarkup += `<div class="${partClass}" style="grid-area:${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;   
         //checking if the snake head hit the body, if so set gameOver to true
         if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
             gameOver = true;
